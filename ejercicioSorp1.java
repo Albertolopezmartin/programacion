8818e06b3a3beee11a95ce03fe3856aee1cfc732
Albertolopezmartin <55782938+Albertolopezmartin@users.noreply.github.com>
2019-10-17 10:23:28 +0200

    Add files via upload

---
+++ ejercicioSorp1.java
@@ -0,0 +1,74 @@
+//Alberto López Martín 1º DAW
+public class ejercicioSorp1 {
+  public static void main (String[] args) {
+    int altura;
+    String caracter;
+    int incrementoAltura;
+    int numVeces;
+    
+    int hParte1;
+    int hParte2;
+    
+    do {
+      System.out.print("Introduzca la altura inicial:");
+      altura = Integer.parseInt(System.console().readLine());
+      if (!((altura >=3) && (altura % 2 == 1))){
+        System.out.print("No es valido; debe ser impar y mayor o igual que 3.");
+      }
+    } while (!((altura >=3) && (altura % 2 == 1))); 
+    
+    System.out.print("Introduzca el caracter a dibujar: ");
+    caracter = System.console().readLine();
+    
+    do {
+      System.out.print("Introduzca el incremento en la altura:");
+      incrementoAltura = Integer.parseInt(System.console().readLine());
+      if (!(incrementoAltura % 2 == 0)){
+        System.out.print("No es valido; debe ser par.");
+      }
+    } while (!(incrementoAltura % 2 == 0)); 
+    
+    System.out.print("Introduzca el numero de veces que desea dibujar el reloj: ");
+    numVeces = Integer.parseInt(System.console().readLine());
+    
+    
+    
+    //dibujo de la piramide invertida hacia abajo
+    
+    
+    for (int vez=0; vez<numVeces; vez++){
+      System.out.println();
+      hParte1 = (altura+1) /2;
+      int anchuraLinea = hParte1*2-1;
+      for (int i=0; i<hParte1; i++){
+        //pinto espacios en blanco
+        for (int j=0; j<i; j++){
+          System.out.print(" ");
+        }
+        // pinto relleno
+        for (int j=0; j<anchuraLinea; j++) {
+          System.out.print(caracter);
+        }
+        System.out.println();
+        anchuraLinea-= 2;
+      }
+      
+      //dibujo de la piramide hacia arriba
+      hParte2 = hParte1-1;
+      anchuraLinea = 3;
+      for (int i=hParte2-1; i>=0; i--){
+        //pinto espacios en blanco
+        for (int j=0; j<i; j++){
+          System.out.print(" ");
+        }
+        // pinto relleno
+        for (int j=0; j<anchuraLinea; j++) {
+          System.out.print(caracter);
+        }
+        System.out.println();
+        anchuraLinea+= 2;
+      }
+      altura += incrementoAltura;
+    }
+  }
+}
