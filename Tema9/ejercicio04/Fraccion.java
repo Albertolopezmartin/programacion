a2626e5a6b6e4493fe649c4247ab6afe807ad2f3
Albertolopezmartin <55782938+Albertolopezmartin@users.noreply.github.com>
2020-01-21 10:37:34 +0100

    Add files via upload

---
+++ Tema9/ejercicio04/Fraccion.java
@@ -0,0 +1,73 @@
+//Alberto López Martín 1ºDAW
+
+public class Fraccion{
+  
+  private int signo;
+  private int denominador;
+  private int numerador;
+  
+  public Fraccion(int n, int d){
+    if (d == 0){
+      System.out.println("Las fracciones no admiten 0 como denominador");
+    } else {
+      if (n * d <0){
+        this.signo = -1;
+      } else {
+        this.signo = 1;
+      }
+      this.numerador = Math.abs(n);
+      this.denominador = Math.abs(d);
+    }
+  }
+  
+  int getNumerador(){
+    return this.numerador;
+  }
+  
+  int getDenominador(){
+    return this.denominador;
+  }
+  
+  public String toString(){
+    if (signo == -1){
+      return "-"+ this.numerador + "/"+ this.denominador;
+    } else {
+      return this.numerador + "/"+ this.denominador;
+    }
+  }
+  
+  public Fraccion invierte(){
+    return new Fraccion(this.signo * this.denominador, this.numerador);
+  }
+  
+  public Fraccion multiplica(int n){
+    return new Fraccion(this.signo * this.numerador * n, this.denominador);
+  }
+  
+  public Fraccion multiplica(Fraccion f){
+    return new Fraccion(this.signo * this.numerador * f.getNumerador(), this.denominador * f.getDenominador());
+  }
+  
+  public Fraccion divide(int n){
+    return new Fraccion(this.signo * this.numerador, this.denominador * n);
+  }
+  
+  public Fraccion divide(Fraccion f){
+    return new Fraccion (this.signo * this.numerador * f.getDenominador(), denominador * f.getNumerador());
+  }
+  
+  public Fraccion simplifica(){
+    int s = this.signo;
+    int n = this.numerador;
+    int d = this.denominador;
+    
+    for (int i = 2; i <Math.min(this.numerador, this.denominador); i++){
+      while (((n % i) == 0) && ((d % i) == 0)) {
+        n /= i;
+        d /= i;
+      }
+    }
+    
+    return new Fraccion(s * n, d);
+  }
+}
